e2105af1b6faa9a675196cf249c279573a0461d5
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2025-10-25 14:50:37 +0600

    setting up backend and loading data from backend

--- bistro_boss_server/index.js
+++ bistro_boss_server/index.js
@@ -1,18 +1,67 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
-
-// middleware 
+// middleware
 app.use(cors());
 app.use(express.json());
 
+// database connection setup would go here
+const username = encodeURIComponent(process.env.DB_USER);
+const password = encodeURIComponent(process.env.DB_PASS);
 
-app.get('/',(req,res) => {
-    res.send('Resturent is live Now')
-})
+const uri = `mongodb+srv://${username}:${password}@cluster0.ar9nb8e.mongodb.net/?appName=Cluster0`;
 
-app.listen(port, ()=> {
-    console.log(`Website is running on ${port}`)
-})
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    
+
+    // Get the database and collection on which to run the operation
+    const menuCollection = client.db("bistroDb").collection("menu");
+    const reviewsCollection = client.db("bistroDb").collection("reviews");
+    app.get("/menu", async (req, res) => {
+      const menuItems = await menuCollection.find().toArray();
+      res.send(menuItems);
+    })
+
+     app.get("/reviews", async (req, res) => {
+      const reviewItems = await reviewsCollection.find().toArray();
+      res.send(reviewItems);
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Resturent is live Now");
+});
+
+app.listen(port, () => {
+  console.log(`Website is running on ${port}`);
+});
